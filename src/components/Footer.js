89c65541fda7b66005a1a326239b3c1f68a9f61b
Tonnie Ishangu <67873141+tonyishangu@users.noreply.github.com>
2021-05-27 09:27:12 +0300

    create form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="footer-container">
+            <section className="footer-subscription">
+                <p className="footer-subscription">
+                    Join the  Adventure Newsletter to recieve our best vacation deals
+                </p>
+                <p className="footer-subscription-text">
+                    you can unsubscribe anytime
+                </p>
+                <div className="input-areas">
+                    <form>
+                        <input type="email" name="email" className="footer-input" placeholder="enter email"/>
+                    </form>
+                </div>
+            </section>
         </div>
     )
 }
